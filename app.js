c7a04930bf0ce70b0c5828b8513a9477b5217730
peter <nyankaung567@gmail.com>
2022-05-01 20:05:45 +0630

    add Procfile,gitignore,update port

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ app.get("/:customListName",(req,res)=>{
       else{
         //show Collections
         res.render("list",{listTitle: foundlist.name, newListItems: foundlist.item})
-        
+
       }
     }
   })
@@ -127,12 +127,14 @@ app.post("/delete",(req,res)=>{
 
 
 
-// app.get("/about", function(req, res){
-//   res.render("about");
-// });
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
 
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+
+app.listen(port, function() {
+  console.log("Server started on port Successfully");
 });
